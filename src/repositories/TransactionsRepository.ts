509c5cab9e7b10a733dc6e6ca32faf571d085c02
diegobonagurio <diego.bonagurio@gmail.com>
2020-04-17 11:50:05 -0300

    Aplicaçao concluida

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
 import Transaction from '../models/Transaction';
+import { response } from 'express';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CretateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+enum Type {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
 }
 
 class TransactionsRepository {
@@ -14,15 +26,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculator(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculator(Type.INCOME);
+    const outcome = this.calculator(Type.OUTCOME);
+    const total = income - outcome;
+    const balance: Balance = { income, outcome, total }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type}: CretateTransaction): Transaction  {
+    const transaction = new Transaction({ title, value, type});
+
+    const { total } = this.getBalance();
+    if(type === Type.OUTCOME && total < value ) {
+      throw Error('Error');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
